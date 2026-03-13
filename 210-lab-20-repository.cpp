b8c0daf0ef93bcec6408f85550456186ffb5350c
dwoo27 <dwooed27@gmail.com>
2026-03-12 19:36:36 -0700

    5 min commit

--- 210-lab-20-repository.cpp
+++ 210-lab-20-repository.cpp
@@ -18,7 +18,7 @@ public:
 			prices[i] = price;
 		}
 	}
-	Chair(int l) {
+	Chair(int l, double p[]) {
 		prices = new double[SIZE];
 		legs = l;
 		for (int i = 0; i < SIZE; i++)
@@ -55,21 +55,16 @@ int main() {
 	chairPtr->print();
 	
 	//creating dynamic chair object with constructor
-	Chair* livingChair = new Chair(3);
-	livingChair->setPrices(525.25, 434.34, 252.52);
+	double livingPrices[] = { 525.25, 434.34, 252.52 };
+	Chair* livingChair = new Chair(3, livingPrices);
 	livingChair->print();
 	delete livingChair;
 	livingChair = nullptr;
 	
 	//creating dynamic array of chair objects
 	Chair* collection = new Chair[SIZE];
-	collection[0].setLegs(4);
-	collection[0].setPrices(441.41, 552.52, 663.63);
-	collection[1].setLegs(4);
-	collection[1].setPrices(484.84, 959.59, 868.68);
-	collection[2].setLegs(4);
-	collection[2].setPrices(626.26, 515.15, 757.57);
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE; i++) {
 		collection[i].print();
+	}
 	return 0;
 }
